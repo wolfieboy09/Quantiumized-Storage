890527ac3a5d428b867efbf88c576b9acf74219a
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-10-16 18:45:32 -0400

    Add more types and colors for item

--- src/main/java/dev/wolfieboy09/qstorage/api/storage/ItemStorageType.java
+++ src/main/java/dev/wolfieboy09/qstorage/api/storage/ItemStorageType.java
@@ -1,21 +1,31 @@
 package dev.wolfieboy09.qstorage.api.storage;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.util.StringRepresentable;
 import org.jetbrains.annotations.NotNull;
 
 public enum ItemStorageType implements StringRepresentable {
-    BASIC(500),
-    ADVANCED(1000);
+    BASIC(128, ChatFormatting.WHITE),
+    ADVANCED(512, ChatFormatting.YELLOW),
+    SUPERIOR(2048, ChatFormatting.AQUA),
+    QUANTUM(8192, ChatFormatting.LIGHT_PURPLE),
+    MULTI_DIMENSIONAL(16384, ChatFormatting.DARK_RED);
 
 
     private final int capacity;
+    private final ChatFormatting color;
 
-    ItemStorageType(int capacity) {
+    ItemStorageType(int capacity, ChatFormatting color) {
         this.capacity = capacity;
+        this.color = color;
     }
 
     public int getCapacity() {
         return capacity;
+    }
+
+    public ChatFormatting getColor() {
+        return color;
     }
 
     @Override
