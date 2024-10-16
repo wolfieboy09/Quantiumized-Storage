8968cfa0f81c5f434a35f2878349524019f7a13b
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-10-16 18:45:13 -0400

    Item color, and other things

--- src/main/java/dev/wolfieboy09/qstorage/item/ItemStorageDisk.java
+++ src/main/java/dev/wolfieboy09/qstorage/item/ItemStorageDisk.java
@@ -5,20 +5,28 @@ import dev.wolfieboy09.qstorage.api.storage.IItemStorageDisk;
 import dev.wolfieboy09.qstorage.api.storage.ItemStorageType;
 import dev.wolfieboy09.qstorage.api.storage.StorageType;
 import net.minecraft.core.NonNullList;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public class ItemStorageDisk extends Item implements IItemStorageDisk {
     private final ItemStorageType storageType;
+    private UUID id;
 
-    public ItemStorageDisk(Properties properties, ItemStorageType storageType) {
-        super(properties);
+    public ItemStorageDisk(ItemStorageType storageType) {
+        super(new Properties().stacksTo(1));
         this.storageType = storageType;
     }
 
-
+    @Override
+    public void setId(UUID id) {
+        this.id = Objects.requireNonNullElseGet(id, () -> UUID.nameUUIDFromBytes(getContents().toString().getBytes()));
+    }
 
     @Override
     public NonNullList<ItemStack> getContents() {
@@ -27,7 +35,7 @@ public class ItemStorageDisk extends Item implements IItemStorageDisk {
 
     @Override
     public UUID getId() {
-        return null;
+        return this.id;
     }
 
     @Override
@@ -37,7 +45,7 @@ public class ItemStorageDisk extends Item implements IItemStorageDisk {
 
     @Override
     public boolean isValid() {
-        return false;
+        return this.id != null && !this.id.equals(UUID.nameUUIDFromBytes(getContents().toString().getBytes()));
     }
 
     @Override
@@ -48,5 +56,10 @@ public class ItemStorageDisk extends Item implements IItemStorageDisk {
     @Override
     public ItemStorageType getStorageType() {
         return this.storageType;
+    }
+
+    @Override
+    public @NotNull Component getName(ItemStack stack) {
+        return Component.translatable(stack.getDescriptionId()).withStyle(this.storageType.getColor());
     }
 }
