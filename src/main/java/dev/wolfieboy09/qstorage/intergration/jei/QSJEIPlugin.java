22da0bf33e20e5f96b42bbf578b1b68c2c6865bf
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-10-19 20:08:25 -0400

    Should fix build errors

--- src/main/java/dev/wolfieboy09/qstorage/intergration/jei/QSJEIPlugin.java
+++ src/main/java/dev/wolfieboy09/qstorage/intergration/jei/QSJEIPlugin.java
@@ -1,11 +1,23 @@
 package dev.wolfieboy09.qstorage.intergration.jei;
 
 import dev.wolfieboy09.qstorage.api.util.ResourceHelper;
+import dev.wolfieboy09.qstorage.intergration.jei.disk_assembeler.DiskAssembelerCategory;
+import dev.wolfieboy09.qstorage.registries.QSBlocks;
+import dev.wolfieboy09.qstorage.registries.QSRecipes;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeHolder;
+import net.minecraft.world.item.crafting.RecipeManager;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.stream.Collectors;
 
 @JeiPlugin
 public class QSJEIPlugin implements IModPlugin {
@@ -16,6 +28,21 @@ public class QSJEIPlugin implements IModPlugin {
 
     @Override
     public void registerCategories(IRecipeCategoryRegistration registration) {
-        IModPlugin.super.registerCategories(registration);
+        IGuiHelper guiHelper = registration.getJeiHelpers().getGuiHelper();
+        registration.addRecipeCategories(new DiskAssembelerCategory(guiHelper));
+    }
+
+    @Override
+    public void registerRecipes(@NotNull IRecipeRegistration registration) {
+        if (Minecraft.getInstance().level == null) return;
+        RecipeManager recipeManager = Minecraft.getInstance().level.getRecipeManager();
+
+        registration.addRecipes(DiskAssembelerCategory.RECIPE_TYPE, recipeManager.getAllRecipesFor(QSRecipes.DISK_ASSEMBLER_TYPE.get()).stream()
+                .map(RecipeHolder::value).collect(Collectors.toList()));
+    }
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+        registration.addRecipeCatalyst(new ItemStack(QSBlocks.DISK_ASSEMBLER.get()), DiskAssembelerCategory.RECIPE_TYPE);
     }
 }
--- src/main/java/dev/wolfieboy09/qstorage/intergration/jei/disk_assembeler/DiskAssembelerCategory.java
+++ src/main/java/dev/wolfieboy09/qstorage/intergration/jei/disk_assembeler/DiskAssembelerCategory.java
@@ -1,6 +1,53 @@
 package dev.wolfieboy09.qstorage.intergration.jei.disk_assembeler;
 
+import dev.wolfieboy09.qstorage.QuantiumizedStorage;
+import dev.wolfieboy09.qstorage.api.util.ResourceHelper;
+import dev.wolfieboy09.qstorage.block.disk_assembler.DiskAssemblerRecipe;
+import dev.wolfieboy09.qstorage.registries.QSBlocks;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class DiskAssembelerCategory implements IRecipeCategory<DiskRe> {
+public class DiskAssembelerCategory implements IRecipeCategory<DiskAssemblerRecipe> {
+    public static final RecipeType<DiskAssemblerRecipe> RECIPE_TYPE = RecipeType.create(QuantiumizedStorage.MOD_ID, "disk_assembler", DiskAssemblerRecipe.class);
+    private final IDrawable background;
+    private final IDrawable icon;
+
+    public DiskAssembelerCategory(@NotNull IGuiHelper guiHelper) {
+        ResourceLocation location = ResourceHelper.asResource("disk_assembler");
+        background = guiHelper.createDrawable(location, 0, 0, 100, 100);
+        icon = guiHelper.createDrawableItemStack(QSBlocks.DISK_ASSEMBLER.asStack());
+    }
+
+    @Override
+    public RecipeType<DiskAssemblerRecipe> getRecipeType() {
+        return RECIPE_TYPE;
+    }
+
+    @Override
+    public Component getTitle() {
+        return QSBlocks.DISK_ASSEMBLER.get().getName();
+    }
+
+    @Override
+    public IDrawable getBackground() {
+        return background;
+    }
+
+    @Override
+    public @Nullable IDrawable getIcon() {
+        return icon;
+    }
+
+    @Override
+    public void setRecipe(IRecipeLayoutBuilder builder, DiskAssemblerRecipe recipe, IFocusGroup focuses) {
+
+    }
 }
