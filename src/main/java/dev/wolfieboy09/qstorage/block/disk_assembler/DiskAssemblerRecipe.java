0f4b66c65af18ec2fa0bc75134234ad581e2ba3d
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-10-20 19:07:34 -0400

    Create the codec, stream codec, add some results, and change the recipe part final

--- src/main/java/dev/wolfieboy09/qstorage/block/disk_assembler/DiskAssemblerRecipe.java
+++ src/main/java/dev/wolfieboy09/qstorage/block/disk_assembler/DiskAssemblerRecipe.java
@@ -1,40 +1,53 @@
 package dev.wolfieboy09.qstorage.block.disk_assembler;
 
 
+import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import dev.wolfieboy09.qstorage.registries.QSRecipes;
+import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.NonNullList;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.item.crafting.Recipe;
-import net.minecraft.world.item.crafting.RecipeSerializer;
-import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.item.crafting.*;
 import net.minecraft.world.level.Level;
 import net.neoforged.neoforge.items.wrapper.RecipeWrapper;
+import net.neoforged.neoforge.network.codec.NeoForgeStreamCodecs;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
 
 @ParametersAreNonnullByDefault
 public record DiskAssemblerRecipe(
         Ingredient diskPort,
         Ingredient diskCasing,
         Ingredient screws,
+        List<Holder<Item>> extras,
         int energyCost,
-        int ticks,
+        int timeInTicks,
         ItemStack result
 ) implements Recipe<RecipeWrapper>, RecipeType<DiskAssemblerRecipe> {
 
     @Override
     public boolean matches(RecipeWrapper recipeWrapper, Level level) {
-        return true;
+        return !level.isClientSide();
     }
 
     @Override
-    public ItemStack assemble(RecipeWrapper recipeWrapper, HolderLookup.Provider provider) {
-        return null;
+    public @NotNull ItemStack assemble(RecipeWrapper recipeWrapper, HolderLookup.Provider provider) {
+        return this.result.copy();
+    }
+
+    @Override
+    public @NotNull ItemStack getResultItem(HolderLookup.Provider provider) {
+        return this.result.copy();
     }
 
     @Override
@@ -43,13 +56,8 @@ public record DiskAssemblerRecipe(
     }
 
     @Override
-    public ItemStack getResultItem(HolderLookup.Provider provider) {
-        return null;
-    }
-
-    @Override
-    public RecipeSerializer<?> getSerializer() {
-        return null;
+    public @NotNull RecipeSerializer<?> getSerializer() {
+        return QSRecipes.DISK_ASSEMBLER_SERIALIZER.get();
     }
 
     @Override
@@ -58,15 +66,41 @@ public record DiskAssemblerRecipe(
     }
 
     public static class Serializer implements RecipeSerializer<DiskAssemblerRecipe> {
+        private static final MapCodec<DiskAssemblerRecipe> CODEC = RecordCodecBuilder.mapCodec(
+                builder -> builder.group(
+                        Ingredient.CODEC.fieldOf("port").forGetter(DiskAssemblerRecipe::diskPort),
+                        Ingredient.CODEC.fieldOf("casing").forGetter(DiskAssemblerRecipe::diskCasing),
+                        Ingredient.CODEC.fieldOf("screws").forGetter(DiskAssemblerRecipe::screws),
+                        BuiltInRegistries.ITEM.holderByNameCodec().listOf().fieldOf("extras").forGetter(DiskAssemblerRecipe::extras),
+                        Codec.INT.fieldOf("energy").forGetter(DiskAssemblerRecipe::energyCost),
+                        Codec.INT.fieldOf("ticks").forGetter(DiskAssemblerRecipe::timeInTicks),
+                        ItemStack.CODEC.fieldOf("result").forGetter(DiskAssemblerRecipe::result)
+                ).apply(builder, DiskAssemblerRecipe::new)
+        );
+
+        private static final StreamCodec<RegistryFriendlyByteBuf, DiskAssemblerRecipe> STREAM_CODEC = NeoForgeStreamCodecs.composite(
+                Ingredient.CONTENTS_STREAM_CODEC, DiskAssemblerRecipe::diskPort,
+                Ingredient.CONTENTS_STREAM_CODEC, DiskAssemblerRecipe::diskCasing,
+                Ingredient.CONTENTS_STREAM_CODEC, DiskAssemblerRecipe::screws,
+                ByteBufCodecs.collection(
+                                NonNullList::createWithCapacity,
+                                ByteBufCodecs.holderRegistry(Registries.ITEM),
+                                4
+                ), DiskAssemblerRecipe::extras,
+                ByteBufCodecs.INT, DiskAssemblerRecipe::energyCost,
+                ByteBufCodecs.INT, DiskAssemblerRecipe::timeInTicks,
+                ItemStack.STREAM_CODEC, DiskAssemblerRecipe::result,
+                DiskAssemblerRecipe::new
+        );
 
         @Override
-        public MapCodec<DiskAssemblerRecipe> codec() {
-            return null;
+        public @NotNull MapCodec<DiskAssemblerRecipe> codec() {
+            return CODEC;
         }
 
         @Override
-        public StreamCodec<RegistryFriendlyByteBuf, DiskAssemblerRecipe> streamCodec() {
-            return null;
+        public @NotNull StreamCodec<RegistryFriendlyByteBuf, DiskAssemblerRecipe> streamCodec() {
+            return STREAM_CODEC;
         }
     }
 }
