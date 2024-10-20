1d50dcc9f6aef40a9749eb398b13cedc67caf436
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-10-20 16:17:07 -0400

    Still makes me mad that it crashes somehow

--- src/main/java/dev/wolfieboy09/qstorage/registries/QSCreativeTab.java
+++ src/main/java/dev/wolfieboy09/qstorage/registries/QSCreativeTab.java
@@ -1,47 +1,34 @@
 package dev.wolfieboy09.qstorage.registries;
 
-import com.tterrag.registrate.util.entry.RegistryEntry;
 import dev.wolfieboy09.qstorage.QuantiumizedStorage;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
-import org.jetbrains.annotations.NotNull;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 import static dev.wolfieboy09.qstorage.QuantiumizedStorage.REGISTRATE;
 
 public class QSCreativeTab {
     private static final DeferredRegister<CreativeModeTab> REGISTER = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, QuantiumizedStorage.MOD_ID);
 
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> MAIN_CREATIVE_TAB = addTab("main_creative_tab", "Quantiumized Storage", QSItems.SILICON::asStack);
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> MAIN_CREATIVE_TAB = addTab("main_creative_tab", "Quantiumized Storage", QSItems.DATA_CRYSTAL::asStack);
 
-    public static @NotNull DeferredHolder<CreativeModeTab, CreativeModeTab> addTab(String id, String name, Supplier<ItemStack> icon) {
-        String itemGroupId = "item_group." + QuantiumizedStorage.MOD_ID + "." + id;
+    public static DeferredHolder<CreativeModeTab, CreativeModeTab> addTab(String id, String name, Supplier<ItemStack> icon) {
+        String itemGroupId = "itemGroup." + QuantiumizedStorage.MOD_ID + "." + id;
         REGISTRATE.addRawLang(itemGroupId, name);
-
-        return REGISTER.register(id, () -> {
-            CreativeModeTab.Builder tabBuilder = CreativeModeTab.builder()
-                    .icon(icon)
-                    .displayItems((parameters, populator) -> {
-                        populator.accept(QSItems.SILICON.get());
-                    })
-                    .title(Component.translatable(itemGroupId));
-            return tabBuilder.build();
-        });
+        CreativeModeTab.Builder tabBuilder = CreativeModeTab.builder()
+                .icon(icon)
+                .displayItems((pParameters, populator) -> {
+                    populator.accept(QSItems.SILICON.get());
+                })
+                .title(Component.translatable(itemGroupId));
+        return REGISTER.register(id, tabBuilder::build);
     }
-
-
-
 
     public static void register(IEventBus eventBus) {
         REGISTER.register(eventBus);
