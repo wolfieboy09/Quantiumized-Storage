3f402087f3554d483e4d7895af91e04ce0840498
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-10-19 21:35:01 -0400

    Simplify things

--- src/main/java/dev/wolfieboy09/qstorage/registries/QSRegistrate.java
+++ src/main/java/dev/wolfieboy09/qstorage/registries/QSRegistrate.java
@@ -1,14 +1,14 @@
 package dev.wolfieboy09.qstorage.registries;
 
 import com.tterrag.registrate.AbstractRegistrate;
+import dev.wolfieboy09.qstorage.QuantiumizedStorage;
 
 public class QSRegistrate extends AbstractRegistrate<QSRegistrate> {
-
-    protected QSRegistrate(String mod_id) {
-        super(mod_id);
+    protected QSRegistrate() {
+        super(QuantiumizedStorage.MOD_ID);
     }
 
-    public static QSRegistrate create(String mod_id) {
-        return new QSRegistrate(mod_id);
+    public static QSRegistrate create() {
+        return new QSRegistrate();
     }
 }
