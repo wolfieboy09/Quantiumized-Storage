3a219461295c800fcc496b91dd4b18cc61e5c906
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-10-22 19:44:15 -0400

    Have block items exist now

--- src/main/java/dev/wolfieboy09/qstorage/registries/QSBlocks.java
+++ src/main/java/dev/wolfieboy09/qstorage/registries/QSBlocks.java
@@ -2,21 +2,37 @@ package dev.wolfieboy09.qstorage.registries;
 
 import dev.wolfieboy09.qstorage.QuantiumizedStorage;
 import dev.wolfieboy09.qstorage.block.disk_assembler.DiskAssemblerBlock;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+import java.util.function.Supplier;
 
 public class QSBlocks {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(QuantiumizedStorage.MOD_ID);
+    public static final DeferredRegister.Items BLOCK_ITEMS = DeferredRegister.createItems(QuantiumizedStorage.MOD_ID);
 
-    public static final DeferredBlock<DiskAssemblerBlock> DISK_ASSEMBLER = BLOCKS.register(
+    public static final DeferredBlock<DiskAssemblerBlock> DISK_ASSEMBLER = registerBlock(
             "disk_assembler",
             () -> new DiskAssemblerBlock(BlockBehaviour.Properties.of().noOcclusion())
     );
 
+    private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
+        DeferredBlock<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block) {
+        BLOCK_ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
     public static void init(IEventBus bus) {
         BLOCKS.register(bus);
+        BLOCK_ITEMS.register(bus);
     }
 }
