2fdf0693d7bc6df46390c9b37a1aabedfd3bd53c
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-10-24 15:17:04 -0400

    for whenever the thing syncs up

---
+++ src/main/java/dev/wolfieboy09/qstorage/block/ItemResultSlot.java
@@ -0,0 +1,16 @@
+package dev.wolfieboy09.qstorage.block;
+
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.neoforge.items.IItemHandler;
+import net.neoforged.neoforge.items.SlotItemHandler;
+
+public class ItemResultSlot extends SlotItemHandler {
+    public ItemResultSlot(IItemHandler itemHandler, int index, int xPosition, int yPosition) {
+        super(itemHandler, index, xPosition, yPosition);
+    }
+
+    @Override
+    public boolean mayPlace(ItemStack stack) {
+        return false;
+    }
+}
