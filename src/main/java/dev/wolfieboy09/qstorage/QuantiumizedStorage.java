7771428b27ef57ffaf198e33e777a4ff037f8e79
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-10-28 14:34:16 -0400

    Remove modcontainer for now

--- src/main/java/dev/wolfieboy09/qstorage/QuantiumizedStorage.java
+++ src/main/java/dev/wolfieboy09/qstorage/QuantiumizedStorage.java
@@ -6,7 +6,6 @@ import dev.wolfieboy09.qstorage.registries.*;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
@@ -20,7 +19,7 @@ public class QuantiumizedStorage {
     public static final String MOD_ID = "qstorage";
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public QuantiumizedStorage(IEventBus modEventBus, ModContainer modContainer) {
+    public QuantiumizedStorage(IEventBus modEventBus) {
         modEventBus.addListener(this::commonSetup);
 
         QSDataComponents.register(modEventBus);
