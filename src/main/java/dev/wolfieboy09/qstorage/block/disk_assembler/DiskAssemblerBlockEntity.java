d213bc95dd4e11a337fc86ffc2248c3da7ff293c
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-10-29 18:28:04 -0400

    yarrhahhahar more thingies

--- src/main/java/dev/wolfieboy09/qstorage/block/disk_assembler/DiskAssemblerBlockEntity.java
+++ src/main/java/dev/wolfieboy09/qstorage/block/disk_assembler/DiskAssemblerBlockEntity.java
@@ -4,6 +4,8 @@ import dev.wolfieboy09.qstorage.block.AbstractEnergyBlockEntity;
 import dev.wolfieboy09.qstorage.registries.QSBlockEntities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
@@ -12,8 +14,10 @@ import net.neoforged.neoforge.energy.EnergyStorage;
 import net.neoforged.neoforge.items.ItemStackHandler;
 import org.jetbrains.annotations.Nullable;
 
+import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.Objects;
 
+@ParametersAreNonnullByDefault
 public class DiskAssemblerBlockEntity extends AbstractEnergyBlockEntity {
     private int progress = 0;
     private int crafting_ticks = 0;
@@ -113,5 +117,35 @@ public class DiskAssemblerBlockEntity extends AbstractEnergyBlockEntity {
         SimpleContainer container = new SimpleContainer(1);
         container.setItem(8, inventory.getStackInSlot(8));
         return container;
+    }
+
+    @Override
+    public void saveExtra(CompoundTag tag, HolderLookup.Provider registries) {
+        super.saveExtra(tag, registries);
+        tag.putInt("energy_required", this.energy_required);
+        tag.putInt("crafting_ticks", this.crafting_ticks);
+        tag.put("inventory", this.inventory.serializeNBT(registries));
+    }
+
+    @Override
+    protected void loadExtra(CompoundTag tag, HolderLookup.Provider registries) {
+        super.loadExtra(tag, registries);
+        this.energy_required = tag.getInt("energy_required");
+        this.crafting_ticks = tag.getInt("crafting_ticks");
+        this.inventory.deserializeNBT(registries, tag.getCompound("inventory"));
+    }
+
+    @Override
+    public CompoundTag updateTag(CompoundTag tag, HolderLookup.Provider lookupProvider) {
+        tag.putInt("energy_required", this.energy_required);
+        tag.putInt("crafting_ticks", this.crafting_ticks);
+        return super.updateTag(tag, lookupProvider);
+    }
+
+    @Override
+    public void handleUpdate(CompoundTag tag, HolderLookup.Provider lookupProvider) {
+        super.handleUpdate(tag, lookupProvider);
+        this.energy_required = tag.getInt("energy_required");
+        this.crafting_ticks = tag.getInt("crafting_ticks");
     }
 }
