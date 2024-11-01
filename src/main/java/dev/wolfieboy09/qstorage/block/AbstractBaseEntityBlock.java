fe02f45d87d47e0569ec696de09741e902892315
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-11-01 12:11:46 -0400

    Add AbstractBaseEntityBlock

---
+++ src/main/java/dev/wolfieboy09/qstorage/block/AbstractBaseEntityBlock.java
@@ -0,0 +1,20 @@
+package dev.wolfieboy09.qstorage.block;
+
+
+import dev.wolfieboy09.qstorage.api.annotation.NothingNullByDefault;
+import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.state.BlockState;
+
+@SuppressWarnings("deprecation")
+@NothingNullByDefault
+public abstract class AbstractBaseEntityBlock extends BaseEntityBlock {
+    public AbstractBaseEntityBlock(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public RenderShape getRenderShape(BlockState state) {
+        return RenderShape.MODEL;
+    }
+}
