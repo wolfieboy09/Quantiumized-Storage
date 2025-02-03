d172175a1da878dd3e4ec1e352dee41a3415b99f
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-02-03 13:32:46 -0500

    I was, uh, testing events, and did not mean to push this

--- src/main/java/dev/wolfieboy09/qstorage/api/events/BlockPlacedEvent.java
+++
@@ -1,9 +0,0 @@
-package dev.wolfieboy09.qstorage.api.events;
-
-import net.neoforged.bus.api.Event;
-
-public class BlockPlacedEvent extends Event {
-    public void doTheThing() {
-        System.out.println("I have been called");
-    }
-}
