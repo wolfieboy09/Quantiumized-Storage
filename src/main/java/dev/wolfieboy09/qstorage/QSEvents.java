d030c9a8cd2446faf30c689acb54990de0cf59a3
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-02-04 12:51:04 -0500

    Add particle event

--- src/main/java/dev/wolfieboy09/qstorage/QSEvents.java
+++ src/main/java/dev/wolfieboy09/qstorage/QSEvents.java
@@ -1,11 +1,18 @@
 package dev.wolfieboy09.qstorage;
 
 import dev.wolfieboy09.qstorage.api.registry.QSRegistries;
+import dev.wolfieboy09.qstorage.api.registry.gas.GasParticleProvider;
+import dev.wolfieboy09.qstorage.registries.QSParticleTypes;
+import net.neoforged.neoforge.client.event.RegisterParticleProvidersEvent;
 import net.neoforged.neoforge.registries.NewRegistryEvent;
 import org.jetbrains.annotations.NotNull;
 
 public class QSEvents {
     public static void registerRegistries(@NotNull NewRegistryEvent event) {
         event.register(QSRegistries.GAS_REGISTRY);
+    }
+
+    public static void particle(@NotNull RegisterParticleProvidersEvent event) {
+        event.registerSpriteSet(QSParticleTypes.GAS_PARTICLE.get(), GasParticleProvider::new);
     }
 }
