24eb9645c2fc9fa1029ab22c9b2af6a77861df70
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-02-04 12:50:50 -0500

    No need to sync

--- src/main/java/dev/wolfieboy09/qstorage/api/registry/QSRegistries.java
+++ src/main/java/dev/wolfieboy09/qstorage/api/registry/QSRegistries.java
@@ -3,6 +3,7 @@ package dev.wolfieboy09.qstorage.api.registry;
 import dev.wolfieboy09.qstorage.QuantiumizedStorage;
 import dev.wolfieboy09.qstorage.api.registry.gas.Gas;
 import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.neoforge.registries.RegistryBuilder;
@@ -10,6 +11,7 @@ import net.neoforged.neoforge.registries.RegistryBuilder;
 public class QSRegistries {
     public static final ResourceKey<Registry<Gas>> GAS_REGISTRY_KEY = ResourceKey.createRegistryKey(ResourceLocation.fromNamespaceAndPath(QuantiumizedStorage.MOD_ID, "gas"));
     public static final Registry<Gas> GAS_REGISTRY = new RegistryBuilder<>(GAS_REGISTRY_KEY)
-            .sync(true)
+            //.sync(true)
             .create();
+
 }
