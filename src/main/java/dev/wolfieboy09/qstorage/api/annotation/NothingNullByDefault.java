b883810540cf0aa0ea4e961934b5c1a805a8a6e6
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-02-07 07:38:42 -0500

    Add javadoc

--- src/main/java/dev/wolfieboy09/qstorage/api/annotation/NothingNullByDefault.java
+++ src/main/java/dev/wolfieboy09/qstorage/api/annotation/NothingNullByDefault.java
@@ -2,10 +2,17 @@ package dev.wolfieboy09.qstorage.api.annotation;
 
 import javax.annotation.Nonnull;
 import javax.annotation.meta.TypeQualifierDefault;
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * Marks all fields, methods and parameters as non-null
+ * <br>
+ * <i>Perfect for making IntelliJ be quiet with adding {@link org.jetbrains.annotations.NotNull} everywhere</i>
+ */
+@Documented
 @Nonnull
 @TypeQualifierDefault({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.CLASS)
