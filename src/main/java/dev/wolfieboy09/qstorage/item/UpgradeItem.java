26a7b570ea7d764ca02853c113dcf8becddfa176
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-03-17 11:57:50 -0400

    Start the upgrade stuff

---
+++ src/main/java/dev/wolfieboy09/qstorage/item/UpgradeItem.java
@@ -0,0 +1,24 @@
+package dev.wolfieboy09.qstorage.item;
+
+import dev.wolfieboy09.qstorage.api.annotation.NothingNullByDefault;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+
+import java.util.List;
+
+@NothingNullByDefault
+public class UpgradeItem extends Item {
+    private final String langKey;
+
+    public UpgradeItem(Properties properties, String tooltipLangKey) {
+        super(properties);
+        this.langKey = tooltipLangKey;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        tooltipComponents.add(Component.translatable(this.langKey));
+    }
+}
