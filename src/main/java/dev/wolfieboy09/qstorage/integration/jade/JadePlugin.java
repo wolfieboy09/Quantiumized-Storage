430e96ee90ac8fabd7dd03c1e856b43596763976
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-03-26 08:55:31 -0400

    Start Jade plugin for stuff

---
+++ src/main/java/dev/wolfieboy09/qstorage/integration/jade/JadePlugin.java
@@ -0,0 +1,8 @@
+package dev.wolfieboy09.qstorage.integration.jade;
+
+import snownee.jade.api.IWailaPlugin;
+import snownee.jade.api.WailaPlugin;
+
+@WailaPlugin
+public class JadePlugin implements IWailaPlugin {
+}
