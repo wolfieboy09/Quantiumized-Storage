e84036d148619608ac745b9a0cf0738c860d0bbe
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-03-27 18:56:09 -0400

    Start the PipeBlock class for stuff

---
+++ src/main/java/dev/wolfieboy09/qstorage/block/PipeBlock.java
@@ -0,0 +1,46 @@
+package dev.wolfieboy09.qstorage.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.material.PushReaction;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class PipeBlock extends Block implements SimpleWaterloggedBlock, EntityBlock {
+    public static final BooleanProperty DOWN = BooleanProperty.create("down");
+    public static final BooleanProperty UP = BooleanProperty.create("up");
+    public static final BooleanProperty NORTH = BooleanProperty.create("north");
+    public static final BooleanProperty SOUTH = BooleanProperty.create("south");
+    public static final BooleanProperty WEST = BooleanProperty.create("west");
+    public static final BooleanProperty EAST = BooleanProperty.create("east");
+    public static final BooleanProperty HAS_DATA = BooleanProperty.create("has_data");
+    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+
+    protected PipeBlock(@NotNull Properties properties) {
+        super(properties.mapColor(MapColor.COLOR_GRAY).strength(0.5f).sound(SoundType.METAL).pushReaction(PushReaction.BLOCK));
+
+        registerDefaultState(stateDefinition.any()
+                .setValue(UP, false)
+                .setValue(DOWN, false)
+                .setValue(NORTH, false)
+                .setValue(SOUTH, false)
+                .setValue(EAST, false)
+                .setValue(WEST, false)
+                .setValue(HAS_DATA, false)
+                .setValue(WATERLOGGED, false)
+        );
+    }
+
+    @Override
+    public @Nullable BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
+        return null;
+    }
+}
