e51f53db33b0efdb1847524a34f2cf7244c22aff
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-03-31 16:52:56 -0400

    Start the GasCanisterItem

---
+++ src/main/java/dev/wolfieboy09/qstorage/item/GasCanisterItem.java
@@ -0,0 +1,35 @@
+package dev.wolfieboy09.qstorage.item;
+
+import dev.wolfieboy09.qstorage.api.capabilities.gas.GasTank;
+import dev.wolfieboy09.qstorage.api.components.GasCanisterComponent;
+import dev.wolfieboy09.qstorage.api.gas.SingleGasTankHandler;
+import dev.wolfieboy09.qstorage.component.QSDataComponents;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
+
+public class GasCanisterItem extends Item {
+    public GasCanisterItem(@NotNull Properties properties) {
+        super(properties.component(QSDataComponents.GAS_CANISTER_COMPONENT, new GasCanisterComponent(new SingleGasTankHandler(new GasTank(5000)))));
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        GasCanisterComponent data = stack.get(QSDataComponents.GAS_CANISTER_COMPONENT.get());
+        if (data != null) {
+            tooltipComponents.add(Component.translatable("qstorage.gas_canister.contains_gas", Component.literal(data.getGasTank().getGas().getGasHolder().getRegisteredName())));
+            tooltipComponents.add(Component.literal(data.getGasTank().getGasAmount() + " / " + data.getGasTank().getCapacity()));
+        }
+        if (!tooltipFlag.hasShiftDown()) {
+            tooltipComponents.add(Component.translatable("qstorage.tooltip.shift_info"));
+        } else {
+            tooltipComponents.add(Component.translatable("qstorage.gas_canister.shifted_info"));
+        }
+    }
+}
