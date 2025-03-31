0764bd5c4a3c9bc82aedc1e0c6b6330c6822e9b7
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-03-31 07:50:20 -0400

    Add some javadoc to the BiHolder

--- src/main/java/dev/wolfieboy09/qstorage/api/BiHolder.java
+++ src/main/java/dev/wolfieboy09/qstorage/api/BiHolder.java
@@ -1,4 +1,20 @@
 package dev.wolfieboy09.qstorage.api;
 
-public record BiHolder<L, R>(L left, R right) {
+import org.jetbrains.annotations.Nullable;
+import com.mojang.datafixers.util.Either;
+/**
+ * Holds a left and right value. (Mojang's {@link Either} but can hold both values)
+ * @param left The left value to hold
+ * @param right The right value to hold
+ * @param <L> The left type
+ * @param <R> The right type
+ */
+public record BiHolder<L, R>(@Nullable L left,@Nullable R right) {
+    public boolean isLeftPresent() {
+        return this.left != null;
+    }
+
+    public boolean isRightPresent() {
+        return this.right != null;
+    }
 }
