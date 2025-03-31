89473884d3ad1790aefd8a4e27e97dcc0056c9a6
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-03-31 18:27:38 -0400

    Create the codec for SingleGasTankHandler

--- src/main/java/dev/wolfieboy09/qstorage/api/gas/SingleGasTankHandler.java
+++ src/main/java/dev/wolfieboy09/qstorage/api/gas/SingleGasTankHandler.java
@@ -1,11 +1,15 @@
 package dev.wolfieboy09.qstorage.api.gas;
 
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import dev.wolfieboy09.qstorage.api.capabilities.gas.GasTank;
 import dev.wolfieboy09.qstorage.api.capabilities.gas.IGasHandler;
 import dev.wolfieboy09.qstorage.api.capabilities.gas.IGasHandlerModifiable;
 import dev.wolfieboy09.qstorage.api.registry.gas.GasStack;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.StreamCodec;
 import net.neoforged.neoforge.common.util.INBTSerializable;
 import org.jetbrains.annotations.UnknownNullability;
 
@@ -14,6 +18,19 @@ import javax.annotation.ParametersAreNonnullByDefault;
 @ParametersAreNonnullByDefault
 public class SingleGasTankHandler implements IGasHandler, IGasHandlerModifiable, INBTSerializable<CompoundTag> {
     private GasTank tank;
+
+    public static final Codec<SingleGasTankHandler> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+        GasTank.CODEC.fieldOf("gas_tank").forGetter(SingleGasTankHandler::getTank)
+    ).apply(instance, SingleGasTankHandler::new));
+
+    public static final StreamCodec<RegistryFriendlyByteBuf, SingleGasTankHandler> STREAM_CODEC = StreamCodec.composite(
+            GasTank.STREAM_CODEC, SingleGasTankHandler::getTank,
+            SingleGasTankHandler::new
+    );
+
+    public SingleGasTankHandler(GasTank tank) {
+        this.tank = tank;
+    }
 
     @Override
     public void setStackInTank(int index, GasStack gasStack) {
@@ -67,5 +84,13 @@ public class SingleGasTankHandler implements IGasHandler, IGasHandlerModifiable,
     @Override
     public void deserializeNBT(HolderLookup.Provider provider, CompoundTag compoundTag) {
         this.tank = this.tank.readFromNBT(provider, compoundTag.getCompound("Gas"));
+    }
+
+    public GasTank getTank() {
+        return this.tank;
+    }
+
+    public GasStack getGas() {
+        return this.tank.getGas();
     }
 }
