d1b510d7658d31aaeb7b686d4ba60b1b0339bcfb
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-03-31 13:08:11 -0400

    Create the GasCanisterComponent

---
+++ src/main/java/dev/wolfieboy09/qstorage/api/components/GasCanisterComponent.java
@@ -0,0 +1,19 @@
+package dev.wolfieboy09.qstorage.api.components;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import dev.wolfieboy09.qstorage.api.gas.SingleGasTankHandler;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.StreamCodec;
+
+public record GasCanisterComponent(SingleGasTankHandler gasTankHandler) {
+    public static final Codec<GasCanisterComponent> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+            SingleGasTankHandler.CODEC.fieldOf("gas_tank").forGetter(GasCanisterComponent::gasTankHandler)
+            ).apply(instance, GasCanisterComponent::new)
+    );
+
+    public static final StreamCodec<RegistryFriendlyByteBuf, GasCanisterComponent> STREAM_CODEC = StreamCodec.composite(
+            SingleGasTankHandler.STREAM_CODEC, GasCanisterComponent::gasTankHandler,
+            GasCanisterComponent::new
+    );
+}
