b5ab003d38ebe3cad71edcad4f42561bf6d65468
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-04-01 10:11:53 -0400

    Jade stuff

--- src/main/java/dev/wolfieboy09/qstorage/item/GasCanisterItem.java
+++ src/main/java/dev/wolfieboy09/qstorage/item/GasCanisterItem.java
@@ -4,10 +4,12 @@ import dev.wolfieboy09.qstorage.api.capabilities.gas.GasTank;
 import dev.wolfieboy09.qstorage.api.components.GasCanisterComponent;
 import dev.wolfieboy09.qstorage.api.gas.SingleGasTankHandler;
 import dev.wolfieboy09.qstorage.component.QSDataComponents;
+import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.neoforged.fml.loading.FMLEnvironment;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -22,14 +24,18 @@ public class GasCanisterItem extends Item {
     @ParametersAreNonnullByDefault
     public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
         GasCanisterComponent data = stack.get(QSDataComponents.GAS_CANISTER_COMPONENT.get());
-        if (data != null) {
+        if (data != null && !data.getGasTank().getGas().isEmpty()) {
             tooltipComponents.add(Component.translatable("qstorage.gas_canister.contains_gas", Component.literal(data.getGasTank().getGas().getGasHolder().getRegisteredName())));
             tooltipComponents.add(Component.literal(data.getGasTank().getGasAmount() + " / " + data.getGasTank().getCapacity()));
         }
-        if (!tooltipFlag.hasShiftDown()) {
-            tooltipComponents.add(Component.translatable("qstorage.tooltip.shift_info"));
-        } else {
-            tooltipComponents.add(Component.translatable("qstorage.gas_canister.shifted_info"));
+
+        // To make jade not show this section on the tooltip
+        if (!(FMLEnvironment.dist.isClient() && Minecraft.getInstance().screen == null)) {
+            if (tooltipFlag.hasShiftDown()) {
+                tooltipComponents.add(Component.translatable("qstorage.gas_canister.shifted_info"));
+            } else {
+                tooltipComponents.add(Component.translatable("qstorage.tooltip.shift_info"));
+            }
         }
     }
 }
