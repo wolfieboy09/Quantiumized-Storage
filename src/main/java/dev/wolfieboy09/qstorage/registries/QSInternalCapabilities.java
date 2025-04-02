cfb338af6ec2aaad5c42343089f85582892df06a
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-04-01 20:15:12 -0400

    Create the gas container cap thingy

--- src/main/java/dev/wolfieboy09/qstorage/registries/QSInternalCapabilities.java
+++ src/main/java/dev/wolfieboy09/qstorage/registries/QSInternalCapabilities.java
@@ -1,9 +1,15 @@
 package dev.wolfieboy09.qstorage.registries;
 
 import dev.wolfieboy09.qstorage.QuantiumizedStorage;
+import dev.wolfieboy09.qstorage.api.capabilities.QSCapabilities;
+import dev.wolfieboy09.qstorage.api.capabilities.gas.IGasHandlerItem;
+import dev.wolfieboy09.qstorage.api.components.GasCanisterComponent;
+import dev.wolfieboy09.qstorage.api.registry.gas.GasStack;
 import dev.wolfieboy09.qstorage.block.disk_assembler.DiskAssemblerBlockEntity;
+import dev.wolfieboy09.qstorage.component.QSDataComponents;
 import dev.wolfieboy09.qstorage.item.ItemStorageDisk;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.capabilities.Capabilities;
@@ -23,6 +29,15 @@ public class QSInternalCapabilities {
         event.registerBlockEntity(Capabilities.ItemHandler.BLOCK, QSBlockEntities.SMELTERY.get(), (block, dir) -> block.getInventory());
         event.registerBlockEntity(Capabilities.FluidHandler.BLOCK, QSBlockEntities.SMELTERY.get(), (block, dir) -> block.getFluidHandler());
 
+        event.registerItem(
+                QSCapabilities.GasStorage.ITEM,
+                (itemStack, context) -> {
+                    GasCanisterComponent handler = itemStack.get(QSDataComponents.GAS_CANISTER_COMPONENT.get());
+                    return handler == null ? null : new GasItemHandlerWrapper(itemStack, handler);
+                },
+                QSItems.GAS_CANISTER
+        );
+
         for (DeferredHolder<Item, ? extends Item> item : QSItems.ITEMS.getEntries()) {
             if (item.get() instanceof ItemStorageDisk) {
                 event.registerItem(
@@ -31,6 +46,60 @@ public class QSInternalCapabilities {
                         item.get()
                 );
             }
+        }
+    }
+
+    public record GasItemHandlerWrapper(ItemStack itemStack, GasCanisterComponent handler) implements IGasHandlerItem {
+        @Override
+        public ItemStack getContainer() {
+            return this.itemStack;
+        }
+
+        @Override
+        public int getTanks() {
+            return 1;
+        }
+
+        @Override
+        public GasStack getGasInTank(int index) {
+            return this.handler.getGasTank().getGas();
+        }
+
+        @Override
+        public int getTankCapacity(int index) {
+            return this.handler.getGasTank().getCapacity();
+        }
+
+        @Override
+        public boolean isGasValid(int index, GasStack gasStack) {
+            return this.handler.getGasTank().isGasValid(index, gasStack);
+        }
+
+
+        // In order to prevent quantum entanglement with the canisters, we must copy the data component
+
+        @Override
+        public int fill(GasStack gasStack, boolean simulate) {
+            GasCanisterComponent component = new GasCanisterComponent(this.handler.gasTankHandler());
+            int fill = component.getGasTank().fill(gasStack, simulate);
+            this.itemStack.set(QSDataComponents.GAS_CANISTER_COMPONENT.get(), component);
+            return fill;
+        }
+
+        @Override
+        public GasStack drain(GasStack gasStack, boolean simulate) {
+            GasCanisterComponent component = new GasCanisterComponent(this.handler.gasTankHandler());
+            GasStack stack = component.getGasTank().drain(gasStack, simulate);
+            this.itemStack.set(QSDataComponents.GAS_CANISTER_COMPONENT.get(), component);
+            return stack;
+        }
+
+        @Override
+        public GasStack drain(int maxDrain, boolean simulate) {
+            GasCanisterComponent component = new GasCanisterComponent(this.handler.gasTankHandler());
+            GasStack stack = component.getGasTank().drain(maxDrain, simulate);
+            this.itemStack.set(QSDataComponents.GAS_CANISTER_COMPONENT.get(), component);
+            return stack;
         }
     }
 }
