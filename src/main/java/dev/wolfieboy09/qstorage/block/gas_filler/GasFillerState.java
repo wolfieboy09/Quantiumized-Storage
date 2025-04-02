47ff8dd79f9212ad1d09608022f444062a0ee558
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-04-02 14:03:08 -0400

    Have the GasFillerState hold an int

--- src/main/java/dev/wolfieboy09/qstorage/block/gas_filler/GasFillerState.java
+++ src/main/java/dev/wolfieboy09/qstorage/block/gas_filler/GasFillerState.java
@@ -1,6 +1,11 @@
 package dev.wolfieboy09.qstorage.block.gas_filler;
 
 public enum GasFillerState {
-    FILL,
-    DRAIN;
+    FILL(0),
+    DRAIN(1);
+
+    final int id;
+    GasFillerState(int id) {
+        this.id = id;
+    }
 }
