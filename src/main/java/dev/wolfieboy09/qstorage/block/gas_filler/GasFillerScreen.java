a103d5c19c5822eff4aff7b5ff79888d213e6559
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-04-02 14:02:50 -0400

    Render the arrow (adjust pos) and move label and title

--- src/main/java/dev/wolfieboy09/qstorage/block/gas_filler/GasFillerScreen.java
+++ src/main/java/dev/wolfieboy09/qstorage/block/gas_filler/GasFillerScreen.java
@@ -17,6 +17,11 @@ public class GasFillerScreen extends AbstractContainerScreen<GasFillerMenu> {
 
     public GasFillerScreen(GasFillerMenu menu, Inventory playerInventory, Component title) {
         super(menu, playerInventory, title);
+        this.titleLabelX = 8;
+        this.titleLabelY = 3;
+
+        this.inventoryLabelX = 8;
+        this.inventoryLabelY = 76;
     }
 
     @Override
@@ -25,7 +30,7 @@ public class GasFillerScreen extends AbstractContainerScreen<GasFillerMenu> {
         CycleButton<GasFillerState> cycler = CycleButton.<GasFillerState>builder((gasFillerState) -> Component.literal(gasFillerState.name()))
                 .withInitialValue(GasFillerState.FILL)
                 .withValues(GasFillerState.FILL, GasFillerState.DRAIN)
-                .create(232, 110, 75, 15, Component.literal("Mode"), (button, value) -> System.out.println("Value: " + value));
+                .create(232, 110, 75, 15, Component.literal("Mode"), (button, value) -> this.menu.updateMode(button.getValue()));
 
         addRenderableWidget(cycler);
     }
@@ -40,5 +45,11 @@ public class GasFillerScreen extends AbstractContainerScreen<GasFillerMenu> {
     public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
         super.render(guiGraphics, mouseX, mouseY, partialTick);
         this.renderTooltip(guiGraphics, mouseX, mouseY);
+
+        if (this.menu.getFillState() == GasFillerState.FILL) {
+            guiGraphics.blit(BACKGROUND_LOCATION, 5, 50, 176, 0, 50, 15);
+        } else {
+            guiGraphics.blit(BACKGROUND_LOCATION, 5, 50, 176, 15, 50, 15);
+        }
     }
 }
