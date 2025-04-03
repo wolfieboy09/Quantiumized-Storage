0f7eaa6316008543a0768615616bd066d3a6628e
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-04-03 19:12:55 -0400

    Have my IDE stop screaming at me

--- src/main/java/dev/wolfieboy09/qstorage/api/BiHolder.java
+++ src/main/java/dev/wolfieboy09/qstorage/api/BiHolder.java
@@ -4,6 +4,8 @@ import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import com.mojang.datafixers.util.Either;
+import org.jetbrains.annotations.UnknownNullability;
+
 /**
  * Holds a left and right value. (Mojang's {@link Either} but can hold both values)
  * @param left The left value to hold
@@ -11,7 +13,7 @@ import com.mojang.datafixers.util.Either;
  * @param <L> The left type
  * @param <R> The right type
  */
-public record BiHolder<L, R>(@Nullable L left,@Nullable R right) {
+public record BiHolder<L, R>(@UnknownNullability L left,@UnknownNullability R right) {
     public boolean isLeftPresent() {
         return this.left != null;
     }
