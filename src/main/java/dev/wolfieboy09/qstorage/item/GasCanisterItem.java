4c039d1614dd49f92db03169425e00bee79affe7
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-04-04 20:19:13 -0400

    Add bar width and color to show amount

--- src/main/java/dev/wolfieboy09/qstorage/item/GasCanisterItem.java
+++ src/main/java/dev/wolfieboy09/qstorage/item/GasCanisterItem.java
@@ -1,8 +1,9 @@
 package dev.wolfieboy09.qstorage.item;
 
+import dev.wolfieboy09.qstorage.api.annotation.NothingNullByDefault;
 import dev.wolfieboy09.qstorage.api.capabilities.gas.GasTank;
 import dev.wolfieboy09.qstorage.api.components.GasCanisterComponent;
-import dev.wolfieboy09.qstorage.api.gas.SingleGasTankHandler;
+import dev.wolfieboy09.qstorage.api.util.ColorUtil;
 import dev.wolfieboy09.qstorage.component.QSDataComponents;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
@@ -12,21 +13,20 @@ import net.minecraft.world.item.TooltipFlag;
 import net.neoforged.fml.loading.FMLEnvironment;
 import org.jetbrains.annotations.NotNull;
 
-import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.List;
 
+@NothingNullByDefault
 public class GasCanisterItem extends Item {
     public GasCanisterItem(@NotNull Properties properties) {
-        super(properties.component(QSDataComponents.GAS_CANISTER_COMPONENT, new GasCanisterComponent(new SingleGasTankHandler(new GasTank(5000)))));
+        super(properties.component(QSDataComponents.GAS_CANISTER_COMPONENT, new GasCanisterComponent((new GasTank(5000)))));
     }
 
     @Override
-    @ParametersAreNonnullByDefault
     public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
-        GasCanisterComponent data = stack.get(QSDataComponents.GAS_CANISTER_COMPONENT.get());
+        GasCanisterComponent data = stack.get(QSDataComponents.GAS_CANISTER_COMPONENT);
         if (data != null && !data.getGasTank().getGas().isEmpty()) {
             tooltipComponents.add(Component.translatable("qstorage.gas_canister.contains_gas", data.getGasTank().getGas().getGas().getName()));
-            tooltipComponents.add(Component.literal(data.getGasTank().getGasAmount() + " / " + data.getGasTank().getCapacity()));
+            tooltipComponents.add(Component.literal(data.getGasTank().getGasAmount() + " / " + data.getGasTank().getCapacity() + " gU"));
         }
 
         // To make jade not show this section on the tooltip
@@ -37,5 +37,31 @@ public class GasCanisterItem extends Item {
                 tooltipComponents.add(Component.translatable("qstorage.tooltip.shift_info"));
             }
         }
+    }
+
+    @Override
+    public boolean isDamageable(ItemStack stack) {
+        return false;
+    }
+
+    @Override
+    public boolean isBarVisible(ItemStack stack) {
+        return true;
+    }
+
+    @Override
+    public int getBarWidth(ItemStack stack) {
+        GasCanisterComponent data = stack.get(QSDataComponents.GAS_CANISTER_COMPONENT);
+        if (data != null) {
+            float fillRatio = (float) data.getGasTank().getGasAmount() / data.getTankCapacity();
+            return Math.round(fillRatio * 13.0F);
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public int getBarColor(ItemStack stack) {
+        return ColorUtil.fromArgb(1, 86, 3, 252);
     }
 }
