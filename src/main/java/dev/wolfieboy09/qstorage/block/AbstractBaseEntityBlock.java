9c95f2d6209368757562231d18ea4aa43d0967b9
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-04-05 06:52:49 -0400

    Remove unneeded things

--- src/main/java/dev/wolfieboy09/qstorage/block/AbstractBaseEntityBlock.java
+++ src/main/java/dev/wolfieboy09/qstorage/block/AbstractBaseEntityBlock.java
@@ -2,13 +2,10 @@ package dev.wolfieboy09.qstorage.block;
 
 
 import dev.wolfieboy09.qstorage.api.annotation.NothingNullByDefault;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.state.BlockState;
 
-@SuppressWarnings("deprecation")
 @NothingNullByDefault
 public abstract class AbstractBaseEntityBlock extends BaseEntityBlock {
     public AbstractBaseEntityBlock(Properties properties) {
@@ -18,10 +15,5 @@ public abstract class AbstractBaseEntityBlock extends BaseEntityBlock {
     @Override
     public RenderShape getRenderShape(BlockState state) {
         return RenderShape.MODEL;
-    }
-
-    @Override
-    protected void onPlace(BlockState state, Level level, BlockPos pos, BlockState oldState, boolean movedByPiston) {
-        super.onPlace(state, level, pos, oldState, movedByPiston);
     }
 }
