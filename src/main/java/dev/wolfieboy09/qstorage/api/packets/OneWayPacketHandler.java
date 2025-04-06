f990097ce20a57aad375220bc36c47c1b82beaec
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-04-06 12:04:52 -0400

    Create the OneWayPacketHandler just because

---
+++ src/main/java/dev/wolfieboy09/qstorage/api/packets/OneWayPacketHandler.java
@@ -0,0 +1,14 @@
+package dev.wolfieboy09.qstorage.api.packets;
+
+import dev.wolfieboy09.qstorage.api.annotation.NothingNullByDefault;
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+import net.neoforged.neoforge.network.handling.IPayloadContext;
+import net.neoforged.neoforge.network.handling.IPayloadHandler;
+
+@NothingNullByDefault
+public record OneWayPacketHandler<T extends CustomPacketPayload>(IPayloadHandler<T> packet) implements IPayloadHandler<T> {
+    @Override
+    public void handle(T payload, IPayloadContext iPayloadContext) {
+        this.packet.handle(payload, iPayloadContext);
+    }
+}
--- src/main/java/dev/wolfieboy09/qstorage/registries/QSPackets.java
+++ src/main/java/dev/wolfieboy09/qstorage/registries/QSPackets.java
@@ -1,11 +1,10 @@
 package dev.wolfieboy09.qstorage.registries;
 
+import dev.wolfieboy09.qstorage.api.packets.OneWayPacketHandler;
 import dev.wolfieboy09.qstorage.block.gas_filler.GasFillerBlockEntity;
 import dev.wolfieboy09.qstorage.packets.GasFillerModeData;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
-import net.neoforged.neoforge.network.handling.DirectionalPayloadHandler;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
-import net.neoforged.neoforge.network.registration.HandlerThread;
 import net.neoforged.neoforge.network.registration.PayloadRegistrar;
 
 public final class QSPackets {
@@ -14,21 +13,12 @@ public final class QSPackets {
         registrar.playToServer(
                 GasFillerModeData.TYPE,
                 GasFillerModeData.STREAM_CODEC,
-                new DirectionalPayloadHandler<>(
-                        ClientPayloadHandler::handleData,
-                        ServerPayloadHandler::handleData
-                )
+                new OneWayPacketHandler<>(ServerPayloadHandler::handleGasFillerMode)
         );
     }
 
-    public static class ClientPayloadHandler {
-        public static void handleData(GasFillerModeData payload, final IPayloadContext context) {
-            // NOOP
-        }
-    }
-
     public static class ServerPayloadHandler {
-        public static void handleData(GasFillerModeData payload, final IPayloadContext context) {
+        public static void handleGasFillerMode(GasFillerModeData payload, final IPayloadContext context) {
             var be = context.player().level().getBlockEntity(payload.blockPos());
             if (be instanceof GasFillerBlockEntity gasFiller) {
                 gasFiller.setState(payload.state());
