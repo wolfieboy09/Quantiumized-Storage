a04f7dcc0f1164bea376821b58a64e299a2f6b48
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-04-15 13:51:40 -0400

    MultiPartBlockStateBuilder stuff for pipes

--- src/main/java/dev/wolfieboy09/qstorage/datagen/QSBlockStateProvider.java
+++ src/main/java/dev/wolfieboy09/qstorage/datagen/QSBlockStateProvider.java
@@ -1,15 +1,23 @@
 package dev.wolfieboy09.qstorage.datagen;
 
 import dev.wolfieboy09.qstorage.QuantiumizedStorage;
+import dev.wolfieboy09.qstorage.api.util.ResourceHelper;
 import dev.wolfieboy09.qstorage.block.disk_assembler.DiskAssemblerBlock;
+import dev.wolfieboy09.qstorage.block.pipe.BasePipeBlock;
 import dev.wolfieboy09.qstorage.block.smeltery.SmelteryBlock;
 import dev.wolfieboy09.qstorage.block.storage_matrix.StorageMatrixBlock;
 import dev.wolfieboy09.qstorage.registries.QSBlocks;
 import net.minecraft.core.Direction;
 import net.minecraft.data.PackOutput;
+import net.minecraft.util.Tuple;
 import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
 import net.neoforged.neoforge.client.model.generators.ConfiguredModel;
+import net.neoforged.neoforge.client.model.generators.ModelFile;
+import net.neoforged.neoforge.client.model.generators.MultiPartBlockStateBuilder;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
 
 public class QSBlockStateProvider extends BlockStateProvider {
     public QSBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
@@ -62,5 +70,40 @@ public class QSBlockStateProvider extends BlockStateProvider {
                             .rotationY(rotation)
                             .build();
                 });
+
+
+        MultiPartBlockStateBuilder multiPartBuilder = getMultipartBuilder(QSBlocks.ITEM_PIPE.get())
+                .part()
+                .modelFile(existingModelFile("block/pipe_dot"))
+                .addModel()
+                .end();
+        Map.of(
+                BasePipeBlock.NORTH, new Tuple<>(0, 0),
+                BasePipeBlock.EAST, new Tuple<>(0, 90),
+                BasePipeBlock.SOUTH, new Tuple<>(0, 180),
+                BasePipeBlock.WEST, new Tuple<>(0, 270),
+                BasePipeBlock.UP, new Tuple<>(270, 0),
+                BasePipeBlock.DOWN, new Tuple<>(90, 0)
+        ).forEach((property, rotations) -> {
+            multiPartBuilder
+                    .part()
+                    .modelFile(existingModelFile("block/pipe"))
+                    .rotationX(rotations.getA())
+                    .rotationY(rotations.getB())
+                    .addModel()
+                    .condition(property, BasePipeBlock.ConnectionType.PIPE)
+                    .end()
+                    .part()
+                    .modelFile(existingModelFile("block/pipe_connected"))
+                    .rotationX(rotations.getA())
+                    .rotationY(rotations.getB())
+                    .addModel()
+                    .condition(property, BasePipeBlock.ConnectionType.BLOCK)
+                    .end();
+        });
+    }
+
+    private ModelFile.@NotNull ExistingModelFile existingModelFile(String path) {
+        return models().getExistingFile(ResourceHelper.asResource(path));
     }
 }
