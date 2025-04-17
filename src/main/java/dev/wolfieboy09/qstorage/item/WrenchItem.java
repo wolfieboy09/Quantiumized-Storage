4a310e3563d51c479a3372219a9cda162855dead
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-04-17 12:32:12 -0400

    Get rid of dupelication, and make it faster

--- src/main/java/dev/wolfieboy09/qstorage/item/WrenchItem.java
+++ src/main/java/dev/wolfieboy09/qstorage/item/WrenchItem.java
@@ -2,7 +2,6 @@ package dev.wolfieboy09.qstorage.item;
 
 import dev.wolfieboy09.qstorage.QuantiumizedStorage;
 import dev.wolfieboy09.qstorage.api.annotation.NothingNullByDefault;
-import dev.wolfieboy09.qstorage.block.pipe.BasePipeBlock;
 import dev.wolfieboy09.qstorage.block.pipe.BasePipeBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -12,7 +11,6 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
-import org.jetbrains.annotations.Nullable;
 
 @NothingNullByDefault
 public class WrenchItem extends Item {
@@ -40,7 +38,6 @@ public class WrenchItem extends Item {
         double relY = clickLocation.y() - pos.getY();
         double relZ = clickLocation.z() - pos.getZ();
         Direction targetDirection = determineTargetDirection(clickedFace, relX, relY, relZ);
-        if (targetDirection == null) return super.useOn(context);
 
         QuantiumizedStorage.LOGGER.debug("Wrench used on {}", targetDirection);
         var state = level.getBlockState(pos);
@@ -50,54 +47,35 @@ public class WrenchItem extends Item {
         return super.useOn(context);
     }
 
-    @Nullable
     private Direction determineTargetDirection(Direction clickedFace, double relX, double relY, double relZ) {
-        switch (clickedFace) {
-            case DOWN: // Clicked bottom face (Y=0)
-                if (relX < CENTER_THRESHOLD_MIN) return Direction.WEST;
-                if (relX > CENTER_THRESHOLD_MAX) return Direction.EAST;
-                if (relZ < CENTER_THRESHOLD_MIN) return Direction.NORTH;
-                if (relZ > CENTER_THRESHOLD_MAX) return Direction.SOUTH;
-                return Direction.DOWN; // Clicked center of bottom face
+        return switch (clickedFace) {
+            case UP, DOWN -> getDirectionFromXZ(clickedFace, relX, relZ);
+            case NORTH, SOUTH -> getDirectionFromXY(clickedFace, relX, relY);
+            case WEST, EAST -> getDirectionFromYZ(clickedFace, relZ, relY);
+        };
+    }
 
-            case UP: // Clicked top face (Y=1)
-                if (relX < CENTER_THRESHOLD_MIN) return Direction.WEST;
-                if (relX > CENTER_THRESHOLD_MAX) return Direction.EAST;
-                if (relZ < CENTER_THRESHOLD_MIN) return Direction.NORTH;
-                if (relZ > CENTER_THRESHOLD_MAX) return Direction.SOUTH;
-                return Direction.UP; // Clicked center of top face
+    private Direction getDirectionFromXZ(Direction face, double relX, double relZ) {
+        if (relX < CENTER_THRESHOLD_MIN) return Direction.WEST;
+        if (relX > CENTER_THRESHOLD_MAX) return Direction.EAST;
+        if (relZ < CENTER_THRESHOLD_MIN) return Direction.NORTH;
+        if (relZ > CENTER_THRESHOLD_MAX) return Direction.SOUTH;
+        return face;
+    }
 
-            case NORTH: // Clicked north face (Z=0)
-                if (relX < CENTER_THRESHOLD_MIN) return Direction.WEST;
-                if (relX > CENTER_THRESHOLD_MAX) return Direction.EAST;
-                if (relY < CENTER_THRESHOLD_MIN) return Direction.DOWN;
-                if (relY > CENTER_THRESHOLD_MAX) return Direction.UP;
-                return Direction.NORTH; // Clicked center of north face
+    private Direction getDirectionFromXY(Direction face, double relX, double relY) {
+        if (relX < CENTER_THRESHOLD_MIN) return Direction.WEST;
+        if (relX > CENTER_THRESHOLD_MAX) return Direction.EAST;
+        if (relY < CENTER_THRESHOLD_MIN) return Direction.DOWN;
+        if (relY > CENTER_THRESHOLD_MAX) return Direction.UP;
+        return face;
+    }
 
-            case SOUTH: // Clicked south face (Z=1)
-                if (relX < CENTER_THRESHOLD_MIN) return Direction.WEST;
-                if (relX > CENTER_THRESHOLD_MAX) return Direction.EAST;
-                if (relY < CENTER_THRESHOLD_MIN) return Direction.DOWN;
-                if (relY > CENTER_THRESHOLD_MAX) return Direction.UP;
-                return Direction.SOUTH; // Clicked center of south face
-
-            case WEST: // Clicked west face (X=0)
-                if (relZ < CENTER_THRESHOLD_MIN) return Direction.NORTH;
-                if (relZ > CENTER_THRESHOLD_MAX) return Direction.SOUTH;
-                if (relY < CENTER_THRESHOLD_MIN) return Direction.DOWN;
-                if (relY > CENTER_THRESHOLD_MAX) return Direction.UP;
-                return Direction.WEST; // Clicked center of west face
-
-            case EAST: // Clicked east face (X=1)
-                if (relZ < CENTER_THRESHOLD_MIN) return Direction.NORTH;
-                if (relZ > CENTER_THRESHOLD_MAX) return Direction.SOUTH;
-                if (relY < CENTER_THRESHOLD_MIN) return Direction.DOWN;
-                if (relY > CENTER_THRESHOLD_MAX) return Direction.UP;
-                return Direction.EAST; // Clicked center of east face
-
-            default:
-                // Should be impossible
-                return null;
-        }
+    private Direction getDirectionFromYZ(Direction face, double relZ, double relY) {
+        if (relZ < CENTER_THRESHOLD_MIN) return Direction.NORTH;
+        if (relZ > CENTER_THRESHOLD_MAX) return Direction.SOUTH;
+        if (relY < CENTER_THRESHOLD_MIN) return Direction.DOWN;
+        if (relY > CENTER_THRESHOLD_MAX) return Direction.UP;
+        return face;
     }
 }
