c0617043a3626da6562c2bf524db0387940b433c
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-04-24 10:48:52 -0400

    Add javadoc

--- src/main/java/dev/wolfieboy09/qstorage/block/pipe/ConnectionType.java
+++ src/main/java/dev/wolfieboy09/qstorage/block/pipe/ConnectionType.java
@@ -4,11 +4,29 @@ import net.minecraft.util.StringRepresentable;
 import org.jetbrains.annotations.NotNull;
 
 public enum ConnectionType implements StringRepresentable {
+    /**
+     * Has no connection at all
+     */
     NONE,
+
+    /**
+     * Pipe to Pipe connection type
+     */
     PIPE,
+
+    /**
+     * Pipe to connected block (chest for example)
+     */
     BLOCK_NORMAL,
+
+    /**
+     * Connected block to pipe network, extracts resource from the container (chest for example)
+     */
     BLOCK_EXTRACT;
 
+    /**
+     * Variable for {@link ConnectionType#values()}
+     */
     public static final ConnectionType[] VALUES = values();
 
     @Override
