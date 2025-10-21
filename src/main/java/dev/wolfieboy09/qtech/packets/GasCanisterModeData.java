e0db9dff4991543ff59f741fb110d1b4346cc088
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-10-21 16:57:17 -0400

    minor change

--- src/main/java/dev/wolfieboy09/qtech/packets/GasCanisterModeData.java
+++ src/main/java/dev/wolfieboy09/qtech/packets/GasCanisterModeData.java
@@ -7,19 +7,19 @@ import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.neoforged.neoforge.network.codec.NeoForgeStreamCodecs;
+import org.jetbrains.annotations.NotNull;
 
 public record GasCanisterModeData(BlockPos blockPos, GasCanisterState state) implements CustomPacketPayload {
     public static final Type<GasCanisterModeData> TYPE = new CustomPacketPayload.Type<>(ResourceHelper.asResource("gas_filler_state"));
 
     public static final StreamCodec<FriendlyByteBuf, GasCanisterModeData> STREAM_CODEC = StreamCodec.composite(
-            BlockPos.STREAM_CODEC,
-            GasCanisterModeData::blockPos,
+            BlockPos.STREAM_CODEC, GasCanisterModeData::blockPos,
             NeoForgeStreamCodecs.enumCodec(GasCanisterState.class), GasCanisterModeData::state,
             GasCanisterModeData::new
     );
 
     @Override
-    public Type<? extends CustomPacketPayload> type() {
+    public @NotNull Type<? extends CustomPacketPayload> type() {
         return TYPE;
     }
 }
