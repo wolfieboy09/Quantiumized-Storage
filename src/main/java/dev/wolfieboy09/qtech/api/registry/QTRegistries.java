3cd7727f835c5872b71b2768cf08f6e49eee2642
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-10-23 09:47:40 -0400

    Add a default key to the gas registry

--- src/main/java/dev/wolfieboy09/qtech/api/registry/QTRegistries.java
+++ src/main/java/dev/wolfieboy09/qtech/api/registry/QTRegistries.java
@@ -3,6 +3,7 @@ package dev.wolfieboy09.qtech.api.registry;
 import dev.wolfieboy09.qtech.QuantiumizedTech;
 import dev.wolfieboy09.qtech.api.registry.gas.Gas;
 import dev.wolfieboy09.qtech.api.registry.multiblock_type.MultiblockType;
+import dev.wolfieboy09.qtech.registries.QTGasses;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
@@ -12,6 +13,7 @@ public class QTRegistries {
     public static final ResourceKey<Registry<Gas>> GAS_KEY = ResourceKey.createRegistryKey(ResourceLocation.fromNamespaceAndPath(QuantiumizedTech.MOD_ID, "gas"));
     public static final Registry<Gas> GAS = new RegistryBuilder<>(GAS_KEY)
             .sync(true)
+            .defaultKey(QTGasses.EMPTY.get().getResourceLocation())
             .create();
 
     public static final ResourceKey<Registry<MultiblockType>> MULTIBLOCK_TYPE_KEY = ResourceKey.createRegistryKey(ResourceLocation.fromNamespaceAndPath(QuantiumizedTech.MOD_ID, "multiblock_type"));
