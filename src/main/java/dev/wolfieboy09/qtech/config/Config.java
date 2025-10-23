3ec90aff56ba7b6126833a3e8ff0ec36e8dd91cd
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-10-23 09:47:57 -0400

    Add a static block for future config

--- src/main/java/dev/wolfieboy09/qtech/config/Config.java
+++ src/main/java/dev/wolfieboy09/qtech/config/Config.java
@@ -11,7 +11,11 @@ import net.neoforged.neoforge.common.ModConfigSpec;
 public class Config {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
-    public static ModConfigSpec SPEC = BUILDER.build();
+    public static ModConfigSpec SPEC;
+
+    static {
+        SPEC = BUILDER.build();
+    }
 
     @SubscribeEvent
     static void onLoad(final ModConfigEvent event) {
