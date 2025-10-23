15539d86463187f9283b9d0859d98fb163136f78
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-10-23 13:13:39 -0400

    Fix bug of it crashing when calling .get()

--- src/main/java/dev/wolfieboy09/qtech/api/multiblock/blocks/BaseMultiblockController.java
+++ src/main/java/dev/wolfieboy09/qtech/api/multiblock/blocks/BaseMultiblockController.java
@@ -3,9 +3,12 @@ package dev.wolfieboy09.qtech.api.multiblock.blocks;
 import dev.wolfieboy09.qtech.api.registry.multiblock_type.MultiblockType;
 import net.minecraft.world.level.block.Block;
 
+import java.util.function.Supplier;
+
 public abstract class BaseMultiblockController extends Block {
-    private final MultiblockType type;
-    public BaseMultiblockController(Properties properties, MultiblockType type) {
+    private final Supplier<MultiblockType> type;
+
+    public BaseMultiblockController(Properties properties, Supplier<MultiblockType> type) {
         super(properties);
         this.type = type;
     }
