10d8191aafa6f712134d545feb543c366358955c
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-10-25 19:55:12 -0400

    Some jade stuff for multiblocks

--- src/main/java/dev/wolfieboy09/qtech/integration/jade/JadePlugin.java
+++ src/main/java/dev/wolfieboy09/qtech/integration/jade/JadePlugin.java
@@ -1,8 +1,23 @@
 package dev.wolfieboy09.qtech.integration.jade;
 
+import dev.wolfieboy09.qtech.api.annotation.NothingNullByDefault;
+import dev.wolfieboy09.qtech.api.multiblock.blocks.BaseMultiblockController;
+import dev.wolfieboy09.qtech.integration.jade.multiblocks.MultiblockComponentProvider;
+import snownee.jade.api.IWailaClientRegistration;
+import snownee.jade.api.IWailaCommonRegistration;
 import snownee.jade.api.IWailaPlugin;
 import snownee.jade.api.WailaPlugin;
 
 @WailaPlugin
+@NothingNullByDefault
 public class JadePlugin implements IWailaPlugin {
+    @Override
+    public void register(IWailaCommonRegistration registration) {
+
+    }
+
+    @Override
+    public void registerClient(IWailaClientRegistration registration) {
+        registration.registerBlockComponent(MultiblockComponentProvider.INSTANCE, BaseMultiblockController.class);
+    }
 }
---
+++ src/main/java/dev/wolfieboy09/qtech/integration/jade/multiblocks/MultiblockComponentProvider.java
@@ -0,0 +1,42 @@
+package dev.wolfieboy09.qtech.integration.jade.multiblocks;
+
+import dev.wolfieboy09.qtech.api.annotation.NothingNullByDefault;
+import dev.wolfieboy09.qtech.api.multiblock.blocks.BaseMultiblockEntityController;
+import dev.wolfieboy09.qtech.api.util.ResourceHelper;
+import net.minecraft.ChatFormatting;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import snownee.jade.api.BlockAccessor;
+import snownee.jade.api.IBlockComponentProvider;
+import snownee.jade.api.IServerDataProvider;
+import snownee.jade.api.ITooltip;
+import snownee.jade.api.config.IPluginConfig;
+
+@NothingNullByDefault
+public enum MultiblockComponentProvider implements IBlockComponentProvider, IServerDataProvider<BlockAccessor> {
+    INSTANCE;
+
+    @Override
+    public void appendTooltip(ITooltip tooltip, BlockAccessor accessor, IPluginConfig iPluginConfig) {
+        if (accessor.getServerData().contains("Formed")) {
+            boolean isFormed = accessor.getServerData().getBoolean("Formed");
+            if (isFormed) {
+                tooltip.append(Component.translatable("multiblock.qtech.formed").withStyle(ChatFormatting.GREEN));
+            } else {
+                tooltip.append(Component.translatable("multiblock.qtech.unformed").withStyle(ChatFormatting.RED));
+            }
+        }
+    }
+
+    @Override
+    public void appendServerData(CompoundTag compoundTag, BlockAccessor blockAccessor) {
+        BaseMultiblockEntityController controller = (BaseMultiblockEntityController) blockAccessor.getBlockEntity();
+        compoundTag.putBoolean("Formed", controller.isFormed());
+    }
+
+    @Override
+    public ResourceLocation getUid() {
+        return ResourceHelper.asResource("multiblock_controller");
+    }
+}
