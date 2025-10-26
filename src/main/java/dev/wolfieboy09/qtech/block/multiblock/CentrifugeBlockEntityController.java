383a0d18672aa651773fdca21410a8d66f395a10
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-10-25 20:09:48 -0400

    oopsies number 2

--- src/main/java/dev/wolfieboy09/qtech/block/multiblock/CentrifugeBlockEntityController.java
+++ src/main/java/dev/wolfieboy09/qtech/block/multiblock/CentrifugeBlockEntityController.java
@@ -1,12 +1,13 @@
 package dev.wolfieboy09.qtech.block.multiblock;
 
-import dev.wolfieboy09.qtech.block.GlobalBlockEntity;
+import dev.wolfieboy09.qtech.api.multiblock.blocks.BaseMultiblockEntityController;
 import dev.wolfieboy09.qtech.registries.QTBlockEntities;
+import dev.wolfieboy09.qtech.registries.QTMultiblockTypes;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.state.BlockState;
 
-public class CentrifugeBlockEntityController extends GlobalBlockEntity {
+public class CentrifugeBlockEntityController extends BaseMultiblockEntityController {
     public CentrifugeBlockEntityController(BlockPos pos, BlockState blockState) {
-        super(QTBlockEntities.CENTRIFUGE_CONTROLLER.get(), pos, blockState);
+        super(QTBlockEntities.CENTRIFUGE_CONTROLLER.get(), QTMultiblockTypes.CENTRIFUGE.get(), pos, blockState);
     }
 }
