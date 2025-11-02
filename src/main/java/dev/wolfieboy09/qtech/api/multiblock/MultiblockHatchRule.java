62844e57cf91715fefa410003e3f3bd7ac1f5fe6
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-11-02 08:13:17 -0500

    Add isBothAllowed for the MultiblockHatchRule

--- src/main/java/dev/wolfieboy09/qtech/api/multiblock/MultiblockHatchRule.java
+++ src/main/java/dev/wolfieboy09/qtech/api/multiblock/MultiblockHatchRule.java
@@ -4,6 +4,10 @@ import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 public record MultiblockHatchRule(boolean allowInsert, boolean allowExtract) {
+    public boolean isBothAllowed() {
+        return this.allowInsert && this.allowExtract;
+    }
+
     @Contract(" -> new")
     public static @NotNull MultiblockHatchRule insertOnly() {
         return new MultiblockHatchRule(true, false);
