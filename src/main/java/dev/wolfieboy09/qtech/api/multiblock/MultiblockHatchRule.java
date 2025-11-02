38f7583a19b4eb8859a7574e723cd5a55b8c454b
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-11-01 22:07:04 -0400

    Create MultiblockHatchRule

---
+++ src/main/java/dev/wolfieboy09/qtech/api/multiblock/MultiblockHatchRule.java
@@ -0,0 +1,21 @@
+package dev.wolfieboy09.qtech.api.multiblock;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+public record MultiblockHatchRule(boolean allowInsert, boolean allowExtract) {
+    @Contract(" -> new")
+    public static @NotNull MultiblockHatchRule insertOnly() {
+        return new MultiblockHatchRule(true, false);
+    }
+
+    @Contract(" -> new")
+    public static @NotNull MultiblockHatchRule extractOnly() {
+        return new MultiblockHatchRule(false, true);
+    }
+
+    @Contract(" -> new")
+    public static @NotNull MultiblockHatchRule allowBoth() {
+        return new MultiblockHatchRule(true, true);
+    }
+}
