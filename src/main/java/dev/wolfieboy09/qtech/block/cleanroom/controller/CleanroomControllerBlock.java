0b0c7ffcb87af856e108cc7ad07fa2b6b984859c
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-11-10 19:05:14 -0500

    TODO: get cleaning going

--- src/main/java/dev/wolfieboy09/qtech/block/cleanroom/controller/CleanroomControllerBlock.java
+++ src/main/java/dev/wolfieboy09/qtech/block/cleanroom/controller/CleanroomControllerBlock.java
@@ -5,10 +5,13 @@ import dev.wolfieboy09.qtech.api.annotation.NothingNullByDefault;
 import dev.wolfieboy09.qtech.api.cleanroom.CleanroomTracker;
 import dev.wolfieboy09.qtech.block.AbstractBaseEntityBlock;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.Nullable;
 
 @NothingNullByDefault
@@ -35,5 +38,14 @@ public class CleanroomControllerBlock extends AbstractBaseEntityBlock {
             CleanroomTracker.unregister(level, controller.getCurrentInstance());
         }
         super.onRemove(state, level, pos, newState, movedByPiston);
+    }
+
+    @Override
+    protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hitResult) {
+        if (level.isClientSide) return InteractionResult.PASS;
+        if (level.getBlockEntity(pos) instanceof CleanroomControllerBlockEntity controller && !controller.isFullyClean()) {
+            controller.tryFormCleanroom();
+        }
+        return super.useWithoutItem(state, level, pos, player, hitResult);
     }
 }
